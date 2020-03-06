d7edb36be71b8fe7cf25ef67cbc2710d5ef8eb2b
Rishi Jain <47409828+Rishi2809@users.noreply.github.com>
2020-03-06 12:28:25 +0530

    added shimmer view

--- app/src/main/java/com/rishi/loginfirebase/GetDataActivity.java
+++ app/src/main/java/com/rishi/loginfirebase/GetDataActivity.java
@@ -6,11 +6,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.cooltechworks.views.shimmer.ShimmerRecyclerView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,7 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetDataActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
+    ShimmerRecyclerView shimmerRecycler;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference;
     Button b1;
@@ -33,19 +35,26 @@ public class GetDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_data);
-        recyclerView = findViewById(R.id.recycler);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        shimmerRecycler = (ShimmerRecyclerView) findViewById(R.id.shimmer_recycler_view);
+        shimmerRecycler.showShimmerAdapter();
+//        recyclerView = findViewById(R.id.recycler);
+        shimmerRecycler.setHasFixedSize(true);
+        shimmerRecycler.setLayoutManager(new LinearLayoutManager(this));
         mlist = new ArrayList<>();
         uid = getIntent().getStringExtra("uid");
-        Log.d("GetDataActivity",uid);
-        b1 = findViewById(R.id.retrive);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Log.d("GetDataActivity", uid);
                 getData();
-            }
-        });
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        adapter = new MyListAdapter(mlist);
+                        shimmerRecycler.setAdapter(adapter);
+                        shimmerRecycler.hideShimmerAdapter();
+                    }
+                },4000);
+
+
     }
 
     public void getData() {
@@ -76,8 +85,7 @@ public class GetDataActivity extends AppCompatActivity {
 
             }
         });
-        adapter = new MyListAdapter(mlist);
-        recyclerView.setAdapter(adapter);
+
     }
 
 }
